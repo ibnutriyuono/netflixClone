15fe2b918bdc5a5742f5074b597b4e08f8f4ad5f
unknown <ibnutriyuono@gmail.com>
2020-07-27 19:26:41 +0700

    adding banner genre text

---
+++ src/components/BannerGenre.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { globalStyles, colors, stockImages } from "../constants";
+
+const BannerGenre = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.bannerGenreText}>Hearfelt</Text>
+      <Text style={styles.bannerBull}>&bull; </Text>
+      <Text style={styles.bannerGenreText}>Quirky</Text>
+      <Text style={styles.bannerBull}>&bull; </Text>
+      <Text style={styles.bannerGenreText}>Romantic</Text>
+      <Text style={styles.bannerBull}>&bull; </Text>
+      <Text style={styles.bannerGenreText}>Fantasy TV</Text>
+      <Text style={styles.bannerBull}>&bull; </Text>
+      <Text style={styles.bannerGenreText}>Comedy</Text>
+      <Text style={styles.bannerBull}>&bull; </Text>
+      <Text style={styles.bannerGenreText}>Ghost</Text>
+    </View>
+  );
+};
+
+export default BannerGenre;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 20,
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  bannerGenreText: {
+    color: colors.white,
+    fontWeight: "100",
+    marginHorizontal: 20,
+  },
+  bannerBull: {
+    color: colors.infoGrey,
+    fontWeight: "100",
+    marginHorizontal: 20,
+  },
+});
