5c67189eee6c4e6b792229ec79c04a0a93899b0e
unknown <ibnutriyuono@gmail.com>
2020-07-27 19:28:18 +0700

    adding stockimages

--- src/constants/stockImages.js
+++ src/constants/stockImages.js
@@ -1,9 +1,11 @@
 const netflix = require("../assets/images/netflix-logo.png");
 const netflixTransparent = require("../assets/images/netflix-logo-transparent.png");
 const netflixFull = require("../assets/images/netflix-logo-full.png");
+const bannerPoster = require("../assets/images/banner-poster.jpg");
 
 export default {
   netflix,
   netflixTransparent,
   netflixFull,
+  bannerPoster,
 };
