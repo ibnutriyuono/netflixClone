cdab0ca6bb6034483aa8fe29cb10a890364c8afa
unknown <ibnutriyuono@gmail.com>
2020-07-28 11:16:28 +0700

    adding bottom navigation

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import Home from "./src/screens/Home";
-
+import BottomTabNavigator from "./src/routes/bottomTabNavigator";
 export default class App extends Component {
   render() {
-    return <Home />;
+    return <BottomTabNavigator />;
   }
 }
