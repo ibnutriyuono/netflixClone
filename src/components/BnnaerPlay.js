3fe1ea93f7daaf243199f2023335327cb4651f16
unknown <ibnutriyuono@gmail.com>
2020-07-28 07:38:37 +0700

    initial commit

---
+++ src/components/BnnaerPlay.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const BnnaerPlay = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default BnnaerPlay;
+
+const styles = StyleSheet.create({});
