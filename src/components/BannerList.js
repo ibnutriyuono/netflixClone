ee718c9e1e2ad49cb4555eeea062eb4d7c436a0d
unknown <ibnutriyuono@gmail.com>
2020-07-31 20:08:37 +0700

    adding dispatch reducer to bannerlist component

--- src/components/BannerList.js
+++ src/components/BannerList.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { useDispatch } from "react-redux";
+import { addToMyList, removeFromMyList } from "../actions/ListAction";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import { colors, fonts } from "../constants";
 
 const BannerList = (props) => {
   const [pressed, setPress] = useState(true);
+  const dispatch = useDispatch();
+
+  _handlePress = () => {
+    setPress(!pressed);
+    pressed ? dispatch(addToMyList()) : dispatch(removeFromMyList());
+  };
+
   return (
-    <TouchableOpacity onPress={() => setPress(!pressed)}>
+    <TouchableOpacity onPress={() => _handlePress()}>
       <Icon
         name={pressed ? "plus" : "check"}
         size={25}
