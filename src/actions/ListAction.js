59a3945a516de7418ef27f6dc1a0ae52c3698df3
unknown <ibnutriyuono@gmail.com>
2020-07-31 20:07:50 +0700

    adding listaction

---
+++ src/actions/ListAction.js
@@ -0,0 +1,11 @@
+export const addToMyList = () => {
+  return {
+    type: "ADD_TO_MY_LIST",
+  };
+};
+
+export const removeFromMyList = () => {
+  return {
+    type: "REMOVE_FROM_MY_LIST",
+  };
+};
