46d104987ae02a9a851c654640356678f5d6333c
unknown <ibnutriyuono@gmail.com>
2020-07-31 20:09:02 +0700

    adding redux store

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
 import Home from "./src/screens/Home";
 import BottomTabNavigator from "./src/routes/bottomTabNavigator";
+import allReducers from "./src/reducers/";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+const store = createStore(allReducers);
 export default class App extends Component {
   render() {
-    return <BottomTabNavigator />;
+    return (
+      <Provider store={store}>
+        <BottomTabNavigator />
+      </Provider>
+    );
   }
 }
