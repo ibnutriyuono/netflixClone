f6334094fd610b68b1ec84b96e1834fec360bd0f
unknown <ibnutriyuono@gmail.com>
2020-08-05 07:16:20 +0700

    adding previewsscroll component props data

--- src/components/PreviewsScroll.js
+++ src/components/PreviewsScroll.js
@@ -1,28 +1,43 @@
-import React from "react";
-import { StyleSheet, Text, View, ScrollView, Image } from "react-native";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 import { globalStyles, colors, stockImages, fonts } from "../constants";
 
 const PreviewsScroll = (props) => {
+  const state = useSelector((state) => state);
+
   return (
     <View style={styles.previewsContainer}>
       <Text style={styles.previewsText}>{props.heading}</Text>
       <ScrollView horizontal>
-        <Image
-          source={stockImages.netflixTransparent}
-          style={styles.previewsImage}
-        />
-        <Image
-          source={stockImages.netflixTransparent}
-          style={styles.previewsImage}
-        />
-        <Image
-          source={stockImages.netflixTransparent}
-          style={styles.previewsImage}
-        />
-        <Image
-          source={stockImages.netflixTransparent}
-          style={styles.previewsImage}
-        />
+        {props.data.results.map((item, index) => {
+          return (
+            <View key={index}>
+              <TouchableOpacity onPress={() => console.log(item.id)}>
+                <View>
+                  <Image
+                    source={{
+                      uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`,
+                    }}
+                    style={styles.previewsImage}
+                  />
+                  <Text style={styles.title}>
+                    {item.original_title.length > 19
+                      ? `${item.original_title.substring(0, 20 - 3)} ...`
+                      : item.original_title}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          );
+        })}
       </ScrollView>
     </View>
   );
@@ -46,5 +61,11 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
     marginVertical: 20,
     borderRadius: 70,
+  },
+  title: {
+    color: colors.white,
+    fontFamily: fonts.medium,
+    fontSize: 15,
+    alignSelf: "center",
   },
 });
