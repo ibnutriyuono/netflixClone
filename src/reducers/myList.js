e0c0de0847a4becbfe07901786a8ee29525152ca
unknown <ibnutriyuono@gmail.com>
2020-08-05 19:28:47 +0700

    adding remove from state array

--- src/reducers/myList.js
+++ src/reducers/myList.js
@@ -1,11 +1,13 @@
-const myListReducer = (state = 0, action) => {
+const myListReducer = (state = [], action) => {
   switch (action.type) {
     case "ADD_TO_MY_LIST":
-      return state + action.payload;
+      // return state + action.payload;
+      return [...state, action.payload];
 
     case "REMOVE_FROM_MY_LIST":
-      return state - 1;
-    // return state;
+      // return state - 1;
+      // return state.filter((e) => e !== action.payload);
+      return [...state.filter((a) => action.payload !== action.payload)];
     default:
       return state;
   }
