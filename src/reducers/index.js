1fbd272d497ada7cb3c9cbdff6f023b7cd09f549
unknown <ibnutriyuono@gmail.com>
2020-08-05 07:15:23 +0700

    adding movie reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import isLoggedReducer from "./isLogged";
 import myListReducer from "./myList";
+import moviesActionReducer from "./moviesAction";
 import { combineReducers } from "redux";
 
 const allReducers = combineReducers({
   isLogged: isLoggedReducer,
   myList: myListReducer,
+  movieAction: moviesActionReducer,
 });
 
 export default allReducers;
